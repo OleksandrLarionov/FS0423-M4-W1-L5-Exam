f10008da961fb6f1bd4faf0d68517f27b326474a
OleksandrLarionov <larionov.alex23@gmail.com>
2023-12-01 12:28:22 +0100

    add entity video properties n testing in main

--- src/Lettore.java
+++ src/Lettore.java
@@ -1,7 +1,26 @@
+import entities.ElementoMultimediale;
+import entities.Video;
+
 import java.util.Scanner;
 
 public class Lettore {
     public static void main(String[] args) {
+        ElementoMultimediale[] momoriaSdDelPlayer = new ElementoMultimediale[5];
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci un titolo");
+        String titolo = scanner.nextLine();
+
+        System.out.println("Inserisci la durata");
+        int durata = scanner.nextInt();
+
+        System.out.println("Inserisci il volume");
+        int volume = scanner.nextInt();
+
+        System.out.println("Inserisci la luminosita");
+        int luminosita = scanner.nextInt();
+
+        Video newVideoUno = new Video(titolo, durata, volume, luminosita);
+        newVideoUno.play();
     }
 }
--- src/entities/Video.java
+++ src/entities/Video.java
@@ -1,17 +1,63 @@
 package entities;
 
-public class Video extends ElementoMultimediale{
-    public Video(String titolo, int durata){
+import interfaces.Luminosita;
+import interfaces.Volume;
+
+public class Video extends ElementoMultimediale implements Volume, Luminosita {
+    private int volume;
+    private int luminosita;
+
+    public Video(String titolo, int durata, int volume, int luminosita) {
         super(titolo, durata);
+        this.volume = volume;
+        this.luminosita = luminosita;
     }
 
     @Override
     public void play() {
-        System.out.println(titolo + durata);
+        System.out.println("Il titolo del video è:" +
+                " " + titolo.toUpperCase() + "." +
+                " " + "Il volume è impostato a:" +
+                " " + "!".repeat(volume) + " " + "-" +
+                " " + "La luminosità è impostata a:" +
+                " " + "*".repeat(luminosita) + ".");
     }
 
     @Override
     public void show() {
         System.out.println("Questa non è un Immagine");
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" +
+                "titolo='" + titolo + '\'' +
+                ", durata=" + durata +
+                '}';
+    }
+
+    // override dell'interface
+    @Override
+    public void aumentaLuminosita() {
+        luminosita++;
+    }
+
+    @Override
+    public void diminuisciLuminosita() {
+        if (luminosita > 0) {
+            luminosita--;
+        }
+    }
+
+    @Override
+    public void abbassaVolume() {
+        if (volume > 0) {
+            volume--;
+        }
+    }
+
+    @Override
+    public void alzaVolume() {
+        volume++;
     }
 }
